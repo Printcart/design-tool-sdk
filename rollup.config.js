5892a2498aaf3b18e5eca4a428c3dd0974499d10
Hiep Vu <hiepv511@gmail.com>
2021-12-31 16:32:32 +0700

    Add dist file minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const dev = (process.env.NODE_ENV !== 'production');
 
 export default {
   input: "main.js",
@@ -12,6 +15,18 @@ export default {
   plugins: [
     replace({
       __DESIGN_TOOL_URL__: JSON.stringify(process.env.DESIGN_TOOL_URL),
+    }),
+    terser({
+      ecma: 2018,
+      mangle: { toplevel: true },
+      compress: {
+        module: true,
+        toplevel: true,
+        unsafe_arrows: true,
+        drop_console: !dev,
+        drop_debugger: !dev
+      },
+      output: { quote_style: 1 }
     })
   ]
 };
